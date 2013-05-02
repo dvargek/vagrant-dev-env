6611581185731a57a347663f4900eaf85d429894
smaerker <sebastien.maerker@googlemail.com>
2013-05-02 10:56:47 +0200

    Modified Vagrantfile and site.pp

--- oxid_test/manifests/site.pp
+++ oxid_test/manifests/site.pp
@@ -1,13 +1,18 @@
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
 class update {
+
+  #notice( "Updating packagelist ... please wait." )
+
   exec { "apt-get update":
     command => "/usr/bin/apt-get update"
   }
+
 }
 
-
 class apache2 {
+
+  #notice( "Installing Apache and PHP ... please wait." )
 
   package { "apache2":
     ensure => present,
@@ -19,14 +24,45 @@ class apache2 {
     notify => Service["apache2"],
     require => Package["apache2"],
   }
-  
+
   service { "apache2":
     ensure => running,
     hasrestart => true,
     require => Package["apache2","libapache2-mod-php5"],
   }
 
+  file { "/etc/apache2/httpd.virtual.conf":
+    ensure  => present,
+    owner => "root",
+    group => "root",
+    mode => 0644,
+    content => template( "httpd.virtual.conf.erb" ),
+    require => Package[ "apache2" ],
+  }
+
+  file { "/etc/apache2/apache2.conf":
+    ensure  => present,
+    owner => "root",
+    group => "root",
+    mode => 0644,
+    content => template( "apache2.conf.erb" ),
+    require => Package[ "apache2" ],
+  }
+
 }
+
+stage { "first":
+    before => Stage[ "main" ],
+}
+
+stage { "last":
+
+}
+
+Stage[ "main" ] -> Stage[ "last" ]
+
 
 include update
 include apache2
+
+#notice( "Everything ist done. Enjoy your development environment." )
