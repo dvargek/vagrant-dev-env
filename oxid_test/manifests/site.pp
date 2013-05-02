e5fe49325031a8201ca07b2a110e9bd8d912b836
smaerker <sebastien.maerker@googlemail.com>
2013-05-02 17:46:44 +0200

    Support Mysql Puppet.

--- oxid_test/manifests/site.pp
+++ oxid_test/manifests/site.pp
@@ -2,8 +2,8 @@ Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
 class update {
 
-  #notice( "Updating packagelist ... please wait." )
-
+  notice( "Updating packagelist ... please wait." )
+  
   exec { "apt-get update":
     command => "/usr/bin/apt-get update"
   }
@@ -12,7 +12,7 @@ class update {
 
 class apache2 {
 
-  #notice( "Installing Apache and PHP ... please wait." )
+  notice( "Installing Apache and PHP ... please wait." )  
 
   package { "apache2":
     ensure => present,
@@ -24,10 +24,12 @@ class apache2 {
     notify => Service["apache2"],
     require => Package["apache2"],
   }
-
+  
   service { "apache2":
+    enable => true,
     ensure => running,
     hasrestart => true,
+    hasstatus => true,
     require => Package["apache2","libapache2-mod-php5"],
   }
 
@@ -51,6 +53,38 @@ class apache2 {
 
 }
 
+class mysql {
+
+  notice( "Installing Msql and PHP ... please wait." )
+  
+  package { "mysql-server": 
+    ensure => present,
+    require => Exec["apt-get update"],
+  }
+
+  file { "/etc/mysql/my.cnf":
+    owner => "root", 
+    group => "root",
+    content => template( "my.cnf.5.5.erb" ),
+    notify => Service[ "mysql" ],
+    require => Package[ "mysql-server" ],
+  }
+
+  service { "mysql":
+    enable => true,
+    ensure => running,
+    hasrestart => true,
+    hasstatus => true,
+    require => Package[ "mysql-server" ],
+  }
+
+  #exec { "set-mysql-password":
+  #  unless => "/usr/bin/mysqladmin -uroot -p${mysql_password} status",
+  #  command => "/usr/bin/mysqladmin -uroot password ${mysql_ password}",
+  #  require => Service["mysql"],
+  #}
+}
+
 stage { "first":
     before => Stage[ "main" ],
 }
@@ -64,5 +98,6 @@ Stage[ "main" ] -> Stage[ "last" ]
 
 include update
 include apache2
+include mysql
 
 #notice( "Everything ist done. Enjoy your development environment." )
