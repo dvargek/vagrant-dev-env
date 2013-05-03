20d73fa6f9a4f9d0231c1880b26cebbb94c0147f
smaerker <sebastien.maerker@googlemail.com>
2013-05-03 11:31:50 +0200

    Some changes in Vagrantfile and oxid_bootstrap.

--- oxid_test/oxid_bootstrap.sh
+++ oxid_test/oxid_bootstrap.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-#apt-get update
-#apt-get install -y apache2
-#rm -rf /var/www
-#ln -fs /vagrant/www /var/www
-
-echo "Installing oxid demo data.\n"
-
-exit 0
+echo "Please set default root password:\n"
+echo "Please set default mysql root password:\n"
+echo "Checking out oxid demo data.\n"
+echo "Installing oxid demo database.\n"
+echo "Doing some oxid config settings.\n"
+echo "Installation of demo oxid demo data and vagrant development box done..\n"
+echo "Enjoy!\n"
