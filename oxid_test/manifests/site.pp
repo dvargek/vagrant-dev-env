11e0eb29ce6f713993032bee3f3384d9e660c423
smaerker <sebastien.maerker@googlemail.com>
2013-05-03 11:54:34 +0200

    Modified Vargantfile & site.pp.

--- oxid_test/manifests/site.pp
+++ oxid_test/manifests/site.pp
@@ -2,7 +2,7 @@ Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
 class update {
 
-  notice( "Updating packagelist ... please wait." )
+  #notice( "Updating packagelist ... please wait." )
   
   exec { "apt-get update":
     command => "/usr/bin/apt-get update"
@@ -12,7 +12,7 @@ class update {
 
 class apache2 {
 
-  notice( "Installing Apache and PHP ... please wait." )  
+  #notice( "Installing Apache and PHP ... please wait." )  
 
   package { "apache2":
     ensure => present,
@@ -48,7 +48,6 @@ class apache2 {
     notify => Service["apache2"],	
     require => Package["apache2"],
   }
-
   
   service { "apache2":
     enable => true,
@@ -80,7 +79,7 @@ class apache2 {
 
 class mysql {
 
-  notice( "Installing Mysql and PHP ... please wait." )
+  #notice( "Installing Mysql and PHP ... please wait." )
   
   package { "mysql-server": 
     ensure => present,
@@ -108,15 +107,11 @@ class mysql {
     require => Package[ "mysql-server" ],
   }
 
-  #exec { "set-mysql-password":
-  #  unless => "/usr/bin/mysqladmin -uroot -p${mysql_password} status",
-  #  command => "/usr/bin/mysqladmin -uroot password ${mysql_ password}",
-  #  require => Service["mysql"],
-  #}
 }
 
 stage { "first":
     before => Stage[ "main" ],
+
 }
 
 stage { "last":
@@ -124,7 +119,6 @@ stage { "last":
 }
 
 Stage[ "main" ] -> Stage[ "last" ]
-
 
 include update
 include apache2
