c09d90d8a893a1ac04632ced0f896f869844ca9e
smaerker <sebastien.maerker@googlemail.com>
2013-05-06 09:39:05 +0200

    Check

--- oxid_test/manifests/site.pp
+++ oxid_test/manifests/site.pp
@@ -2,7 +2,8 @@ Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
 class update {
 
-  #notice( "Updating packagelist ... please wait." )
+  notice( "Updating packagelist." )
+
   exec { "apt-get update":
     command => "/usr/bin/apt-get update",
   }
@@ -10,9 +11,12 @@ class update {
 
 class restart-services {
 
+  notice( "Restarting services." )
+
   exec { "service apache2 restart":
     command => "/usr/bin/service apache2 restart",
   }
+
   exec { "service mysql restart":
     command => "/usr/bin/service mysql restart",
   }
@@ -20,23 +24,33 @@ class restart-services {
 }
 
 class pre-base {
+
   class { update: stage => "first" }
+
 }
 
 class base {
+
+  notice( "Installing apache, php, mysql and subversion." )
+
   include apache2 
   include php5
   include mysql5
   include subversion
+
 }
 
 class post-base {
+
   class { restart-services: stage => "last" }
+
 }
 
 
 stage { "first":
+
     before => Stage[ "main" ],
+
 }
 
 stage { "last": }
@@ -46,4 +60,3 @@ Stage[ "main" ] -> Stage[ "last" ]
 include pre-base
 include base
 include post-base
-#notice( "Everything ist done. Enjoy your development environment." )
