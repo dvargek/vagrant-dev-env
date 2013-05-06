c4ca97212451957a46c886dab7c578e6cd842a5b
smaerker <sebastien.maerker@googlemail.com>
2013-05-06 09:54:28 +0200

    Check

--- oxid_test/modules/php5/manifests/init.pp
+++ oxid_test/modules/php5/manifests/init.pp
@@ -2,22 +2,22 @@ class php5 {
 
   package { "libapache2-mod-php5":
     ensure => present,
-    require => Class["apache2"],
+    require => Class[ "apache2" ],
   }
 
   package { "php5-gd":
     ensure => present,
-    require => Class["apache2"],
+    require => Class[ "apache2" ],
   }
 
   package { "php5-curl":
     ensure => present,
-    require => Class["apache2"],
+    require => Class[ "apache2" ],
   }
 
   package { "php5-mysql":
     ensure => present,
-    require => Class["apache2"],
+    require => Class[ "apache2" ],
   }
 
 }
